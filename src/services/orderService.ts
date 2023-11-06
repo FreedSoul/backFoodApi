3be318fe50e542c28b00224143e548089c6d897b
FreedSoul <mattwork07@gmail.com>
2023-11-06 02:53:43 -0500

    added orderService, orderRoute

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,18 +1,18 @@
 import { Pool, QueryResult } from "pg";
 import getConection from '../libs/postgres';
 
-class dishesService {
+class orderService {
     public constructor() { }
     
     public async findOne(id: string) {
         const dbConnect:Pool = await getConection()
-        const res:QueryResult = await dbConnect.query('SELECT * FROM dishes WHERE dish_id = $1', [Number.parseInt(id)])
+        const res:QueryResult = await dbConnect.query('SELECT * FROM orders WHERE order_id = $1', [Number.parseInt(id)])
         // console.log(res.rows);
         return res.rows
     }
     public async find() {
         const dbConnect:Pool = await getConection()
-        const res:QueryResult = await dbConnect.query('SELECT * FROM dishes')
+        const res:QueryResult = await dbConnect.query('SELECT * FROM orders')
         return res.rows;
     }
     //este data viene de la ruta
@@ -21,7 +21,7 @@ class dishesService {
         let auxField:Array<string> = valuesField.map((x, index) => { return `$${index+1}`})
         let values = auxField.join(", ")
         let fields = queryFields.join(", ")
-        const queryJoined = `INSERT INTO dishes (${fields}) VALUES (${values}) `
+        const queryJoined = `INSERT INTO orders (${fields}) VALUES (${values}) `
         // const res:QueryResult = await dbConnect.query(`INSERT INTO dishes (dish_name, available, price, ingredients) VALUES ($1, $2, $3, $4)`, [dish_name, available, price, ingredients])
         const res:QueryResult = await dbConnect.query(queryJoined, valuesField)
         console.log(res);
@@ -30,7 +30,7 @@ class dishesService {
     public async update(queryFields:Array<string>,valuesField:Array<string|number|boolean|Array<string>>) {
         const dbConnect:Pool = await getConection()
         let fields = queryFields.join(", ")
-        const queryJoined:string = `UPDATE dishes SET ${fields} WHERE dish_id = $${valuesField.length} RETURNING *;`
+        const queryJoined:string = `UPDATE orders SET ${fields} WHERE order_id = $${valuesField.length} RETURNING *;`
         // const res:QueryResult = await dbConnect.query(`UPDATE dishes SET dish_name = $1, available = $2, price = $3, ingredients = $4 WHERE dish_id = $5`, [dish_name, price, available, ingredients,dishID])
         const res:QueryResult = await dbConnect.query(queryJoined, valuesField)
         console.log(res)
@@ -38,10 +38,10 @@ class dishesService {
     }
     public async delete(id: string) {
         const dbConnect:Pool = await getConection()
-        const res:QueryResult = await dbConnect.query('DELETE FROM dishes WHERE dish_id = $1 RETURNING *', [Number.parseInt(id)])
+        const res:QueryResult = await dbConnect.query('DELETE FROM orders WHERE order_id = $1 RETURNING *', [Number.parseInt(id)])
         console.log(res)
         return res.rowCount
     }
 }
 
-export { dishesService }
+export { orderService }
